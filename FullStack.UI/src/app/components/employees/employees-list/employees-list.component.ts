d62cbaeacb0a13512c2e0bb9fdfc14ec742c1ddd
Anonymous <asifahmed1132@gmail.com>
2023-07-02 22:41:21 +0600

    Model and design created for show employee on Angular

--- FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Employee } from './../../../model/employee.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.css']
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
+  employees : Employee[] = [];
+  constructor(){
+
+  }
+  ngOnInit(): void {
+
+  }
 
 }
