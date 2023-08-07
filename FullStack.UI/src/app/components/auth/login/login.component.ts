c80ea6ae9a03a51e11d4bd9144e0613d729723ab
Anonymous <asifahmed1132@gmail.com>
2023-08-08 01:06:27 +0600

    Login form with validation created in angular

--- FullStack.UI/src/app/components/auth/login/login.component.ts
+++ FullStack.UI/src/app/components/auth/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm! : FormGroup;
+  backendError : string = "";
+  constructor(
+    private router : Router,
+    private fb : FormBuilder,
+    private toast: NgToastService){}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email : ['',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      password : ['',Validators.required]
+    })
+  }
+
+
+  get validation(){
+    return this.loginForm.controls;
+  }
 
 }
